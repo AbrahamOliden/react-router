d9396151493bea7c8cbafe8203c995e18afef388
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-07 17:55:31 -0600

    import Post component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Home from './routes/Home.jsx';
 import About from './routes/About.jsx';
+import Post from './routes/Post.jsx';
 import Page404 from './routes/Page404.jsx';
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css';
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <Home /> }/>
         <Route path='/about' element={ <About /> }/>
+        <Route path='/posts/:id' element={ <Post /> } />
         <Route path='*' element={ <Page404 /> }/>
       </Routes>
     </Router>
