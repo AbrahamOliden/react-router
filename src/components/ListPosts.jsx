359efac9e60f4c3363b58c5a18fefed52a453851
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-07 00:19:14 -0600

    map posts array and return a render of each element

--- src/components/ListPosts.jsx
+++ src/components/ListPosts.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/css/componentes/card.css';
 import { search } from '../api/api';
 
@@ -11,7 +12,21 @@ const ListPosts = ({url}) => {
 
     return (
         <section className='posts container'>
-
+            {
+            posts.map((post) => {
+                const {id, title, metadescription, categoria} = post;
+                return <Link to={`/posts/${id}`} key={id} className={`post__card post-card--${categoria}`}>
+                    <article>
+                        <h3 className='post-card__title' >
+                            {title}
+                        </h3>
+                        <p className='post-card__meta' >
+                            {metadescription}
+                        </p>
+                    </article>
+                </Link>
+            })
+            }
         </section>
     )
 };
