75b314e048de445330112bb5374911962698ae80
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-06 23:55:01 -0600

    add useState to create posts and setPosts

--- src/components/ListPosts.jsx
+++ src/components/ListPosts.jsx
@@ -1,6 +1,13 @@
+import { useState, useEffect } from 'react';
 import '../assets/css/componentes/card.css';
+import { search } from '../api/api';
 
 const ListPosts = ({url}) => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        search(url, setPosts);
+    }, [url]);
 
     return (
         <section className='posts container'>
