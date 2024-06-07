ba9364242c064940f11ab8a74bb9d04ddf75cb44
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-06 20:13:56 -0600

    add Page404 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './routes/Home.jsx'
-import About from './routes/About.jsx'
-import './assets/css/base/base.css'
-import './assets/css/componentes/card.css'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './routes/Home.jsx';
+import About from './routes/About.jsx';
+import Page404 from './routes/Page404.jsx';
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css';
+import './App.css';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />} />
+        <Route path='/' element={ <Home /> }/>
+        <Route path='/about' element={ <About /> }/>
+        <Route path='*' element={ <Page404 /> }/>
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
