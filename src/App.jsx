a0438df9924f7e6fd4d81f153b84298bf365dab0
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-06 19:37:15 -0600

    create first version of the App

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Home from './routes/Home.jsx'
+import About from './routes/About.jsx'
+import './assets/css/base/base.css'
+import './assets/css/componentes/card.css'
+import './App.css'
+
+function App() {
+
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
