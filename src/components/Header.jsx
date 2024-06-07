085e8db75e75d23ea1ad731ddd6590dc809b9b09
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-06 21:31:17 -0600

    replace a tags for Link tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import image from '../assets/img/doguito.svg';
 import '../assets/css/componentes/header.css';
 
@@ -8,17 +9,17 @@ function Header() {
                 <span className='menu-hamburguer__icon'></span>
             </div>
             <div className='header-container'>
-                <a href="/" className='flex flex--center'>
+                <Link to="/" className='flex flex--center'>
                     <img src={image} alt="doguito" className='header__logo' />
                     <h1 className='header__title'>Petshop</h1>
-                </a>
+                </Link>
             </div>
             <nav className='menu-header' >
                 <ul className='menu-items' >
-                    <li><a href="#" className="menu-item menu-item--entrar">Enter?</a></li>
-                    <li><a href="#" className="menu-item">Products</a></li>
-                    <li><a href="/" className="menu-item">Blog</a></li>
-                    <li><a href="/about" className="menu-item">About</a></li>
+                    <li><Link to="#" className="menu-item menu-item--entrar">Enter?</Link></li>
+                    <li><Link to="#" className="menu-item">Products</Link></li>
+                    <li><Link to="/" className="menu-item">Blog</Link></li>
+                    <li><Link to="/about" className="menu-item">About</Link></li>
                 </ul>
             </nav>
             <div className='menu-header-background' ></div>
