17f676986f88c61ca6d28fc9f31ea1936e796a9c
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-07 19:51:23 -0600

    Add Route for category path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header.jsx';
 import Home from './routes/Home.jsx';
 import About from './routes/About.jsx';
 import Post from './routes/Post.jsx';
+import Category from './routes/Category.jsx';
 import Page404 from './routes/Page404.jsx';
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css';
@@ -17,6 +18,7 @@ function App() {
         <Route path='/' element={ <Home /> }/>
         <Route path='/about' element={ <About /> }/>
         <Route path='/posts/:id' element={ <Post /> } />
+        <Route path='/category/:id' element={ <Category /> } />
         <Route path='*' element={ <Page404 /> }/>
       </Routes>
     </Router>
