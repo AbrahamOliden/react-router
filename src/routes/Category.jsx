7a2bd8468b742746dbc3ec619f9d453f590f0981
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-07 19:58:27 -0600

    import ListPosts and pass it a url value with useParams

--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 import { search } from "../api/api";
+import { useParams } from "react-router-dom";
 import ListCategories from "../components/ListCategories";
+import ListPosts from "../components/ListPosts";
 import '../assets/css/blog.css';
 
 function Category() {
+    const { id } = useParams();
     return (
         <>
             <div className="container">
                 <h2 className="title-page">Pet news</h2>
             </div>
-            < ListCategories />
+            <ListCategories />
+            <ListPosts url={`/posts?categoria=${id}`} />
         </>
     )
 };
