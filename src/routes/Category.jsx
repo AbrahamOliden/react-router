08d7d6f9c6118d3ad9829d2446ea4eef32d830d1
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-07 22:13:53 -0600

    fixed duplication bug (it was the mf key)

--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { search } from "../api/api";
-import { useParams, Route, Routes } from "react-router-dom";
+import { useParams, Route, Routes, Link, useResolvedPath } from "react-router-dom";
 import ListCategories from "../components/ListCategories";
 import ListPosts from "../components/ListPosts";
 import '../assets/css/blog.css';
@@ -8,11 +8,14 @@ import '../assets/css/blog.css';
 function Category() {
     const [subCategories, setSubCategories] = useState([]);
     const { id } = useParams();
+    const url = useResolvedPath("").pathname;
 
     useEffect(() => {
         search(`/categorias?id=${id}`,(response) => {
             setSubCategories(response[0].subcategorias);
+            console.log(subCategories);
         }); 
+        
     }, [id]);
     return (
         <>
@@ -20,10 +23,20 @@ function Category() {
                 <h2 className="title-page">Pet news</h2>
             </div>
             <ListCategories />
+            <ul className="category-list container flex" >
+                {
+                subCategories.map((subcategory, index) => (
+                    <li className={`category-list__category category-list__category--${id}`} key={index} >
+                        <Link to={`${url}/${subcategory}`} >
+                            {subcategory}
+                        </Link></li>
+                ))
+                }
+            </ul>
+            
             <Routes >
                 <Route path="/" element={<ListPosts url={`/posts?categoria=${id}`} />} />
             </Routes>
-            
         </>
     )
 };
