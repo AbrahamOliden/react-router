46d8356a313e8e52f9d7e359450146719c1e53be
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-07 19:49:05 -0600

    create Category functional component

---
+++ src/routes/Category.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import { search } from "../api/api";
+import '../assets/css/blog.css';
+
+function Category() {
+    return (
+        <>
+            <div className="container">
+                <h2 className="title-page">Pet noticias</h2>
+            </div>
+        </>
+    )
+};
+
+export default Category;
