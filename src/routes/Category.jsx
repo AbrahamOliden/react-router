cd77dac8b6dd751394e50209f72b288c9fcff5c0
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-07 19:53:19 -0600

    import and render ListCategories component

--- src/routes/Category.jsx
+++ src/routes/Category.jsx
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 import { search } from "../api/api";
+import ListCategories from "../components/ListCategories";
 import '../assets/css/blog.css';
 
 function Category() {
     return (
         <>
             <div className="container">
-                <h2 className="title-page">Pet noticias</h2>
+                <h2 className="title-page">Pet news</h2>
             </div>
+            < ListCategories />
         </>
     )
 };
