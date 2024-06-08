5623cf47d52d3d6cd605f49bb2b4a1c3ddd43b6c
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-07 19:03:14 -0600

    import ListCategories

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,16 @@
-import ListPosts from "../components/ListPosts"
+import ListCategories from "../components/ListCategories.jsx";
+import ListPosts from "../components/ListPosts";
 
 const Home = () => {
   return (
     <main>
       <div className="container">
-        <h2 className="title-page">Pet noticias</h2>
+        <h2 className="title-page">Pet news</h2>
       </div>
+      <ListCategories />
       <ListPosts url={'/posts'} />
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
